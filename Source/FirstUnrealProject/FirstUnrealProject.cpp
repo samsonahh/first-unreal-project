2b390c136bc44428d502bdfd5d46f995c2905c23
Samson Wu <78169553+samsonahh@users.noreply.github.com>
2025-03-28 10:06:31 -0400

    cpp

---
+++ Source/FirstUnrealProject/FirstUnrealProject.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FirstUnrealProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FirstUnrealProject, "FirstUnrealProject" );
